0d0772e0fdd40099eaebdf6bf354d00f02ecac7e
Oleksandr Kulahin <oleksandrkulahin@mac.home>
2025-07-20 23:00:30 +0200

    add headless mode to Actions runs

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     baseURL: 'http://localhost:3000',
   },
